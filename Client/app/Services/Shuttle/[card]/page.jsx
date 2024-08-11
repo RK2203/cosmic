8770fd94436d13d18f060c955287da7ac48b7182
Shoal Koley <96832914+RK2203@users.noreply.github.com>
2024-08-11 15:39:46 +0530

    more changes made

--- Client/app/Services/Shuttle/[card]/page.jsx
+++ Client/app/Services/Shuttle/[card]/page.jsx
@@ -7,39 +7,39 @@ export default function page({ params }) {
 	const param = useSearchParams();
 
 	return (
-		<div class="p-6 bg-card w-5/6 text-card-foreground rounded-lg shadow-md relative my-20 bg-gradient-radial from-gray-900 to-white  mx-20">
+		<div class="w-3/4 p-4  bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700 my-20 mx-20">
 			<div className="flex justify-center">
-				<h2 class="text-center text-gray-950 whitespace-pre-line dark:text-gray-900 mb-4 text-4xl">Shuttle No {params.card}</h2>
+				<h2 class="text-center text-gray-200 whitespace-pre-line mb-4 text-4xl">Shuttle No {params.card}</h2>
 			</div>
-			<h2 class="text-lg font-semibold mt-4 ml-5">Here is Your Trip details</h2>
+			<h2 class="text-lg text-gray-200 font-medium mt-4 ml-5">Here is Your Trip details</h2>
 
 			<div class="space-y-2">
 				<div div class="flex items-center ml-6">
-					<div class="w-2 h-2 bg-black rounded-full mr-2"></div>
-					<span className="m-3">25.31 kilometers</span>
-					<div class="w-2 h-2 bg-black rounded-full mr-2"></div>
-					<span className="m-3 ">1 h 28 min</span>
+					<div class="w-2 h-2 bg-white rounded-se-sm mr-2"></div>
+					<span className="m-3 text-gray-200">25.31 kilometers</span>
+					<div class="w-2 h-2 bg-white rounded-se-sm mr-2"></div>
+					<span className="m-3 text-gray-200 ">1 h 28 min</span>
 				</div>
 
 				<div class="flex items-center ml-6">
-				<span className="m-3">FARE : -</span>
-				<span>₹{param.get("fare")}</span>
+				<span className="m-3 text-gray-200 ">FARE : -</span>
+				<span className="text-gray-200">₹{param.get("fare")}</span>
 				</div>
 			</div>
-			<h2 class="text-xl font-semibold mb-4 ml-4 pt-3">Route</h2>
+			<h2 class="text-xl font-medium mb-4 ml-4 pt-3 text-gray-200">Route</h2>
 			<div class="space-y-4">
 				<div class="flex justify-between items-start">
 					<div class="flex items-center">
 						<div class="w-4 h-4 bg-primary rounded-full mr-2"></div>
-						<span>{param.get("start")} Ride Start At : </span>
-						<span className="m-3"> {param.get("startTime")}</span>
+						<span className="text-gray-200">{param.get("start")} Ride Start At : </span>
+						<span className="m-3 text-gray-200"> {param.get("startTime")}</span>
 					</div>
 				</div>
 				<div class="flex justify-between items-start">
 					<div class="flex items-center">
 						<div class="w-4 h-4 bg-primary rounded-full mr-2"></div>
-						<span>{param.get("dest")} Ride end At:</span>
-						<span className="m-3"> {param.get("destTime")}</span>
+						<span className="text-gray-200">{param.get("dest")} Ride end At:</span>
+						<span className="m-3 text-gray-200"> {param.get("destTime")}</span>
 					</div>
 
 				</div>
